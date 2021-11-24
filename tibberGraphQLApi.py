a235e764fc125ea2ae31d86d35cb0548c8eb3600
Alexander <70033307+Alexanderbrandhaug@users.noreply.github.com>
2021-11-25 00:16:51 +0100

    Update tibberGraphQLApi.py

--- tibberGraphQLApi.py
+++ tibberGraphQLApi.py
@@ -1,26 +1,36 @@
 from python_graphql_client import GraphqlClient
 import asyncio
-import os
-import requests
 import time
 
-headers={'Authorization': "5RVds5Cr_tawIiIBb6Itmi6BbVah54xszU62aqA94yQ"}
+
+last_notification_timestamp = 0
+NOTIFICATION_INTERVAL = 5 * 60  # 5min - hvor frekvent du ønsker å bli varslet. 
 
 def print_handle(data):
-    print(data["data"]["liveMeasurement"]["timestamp"]+" "+str(data["data"]["liveMeasurement"]["power"]))
-    tall = (data["data"]["liveMeasurement"]["power"])
-    if tall >= 8000:
+    global last_notification_timestamp
+    print(
+        data["data"]["liveMeasurement"]["timestamp"]
+        + " "
+        + str(data["data"]["liveMeasurement"]["power"])
+    )
+    tall = data["data"]["liveMeasurement"]["power"]
+    current_time = time.time()
+    if (
+        tall >= 1000 #antall watt du ønsker notifikasjon ved
+        and current_time - NOTIFICATION_INTERVAL > last_notification_timestamp
+    ):
         print("OK")
         # schedule async task from sync code
         asyncio.create_task(send_push_notification(data))
-        print("msg sent")
-        asyncio.create_task(sleep())
+        last_notification_timestamp = current_time
+        
+        
 
 client = GraphqlClient(endpoint="wss://api.tibber.com/v1-beta/gql/subscriptions")
 
 query = """
 subscription{
-  liveMeasurement(homeId:"b2b3cc2a-caa5-4185-94a4-9ebf645195e1"){
+  liveMeasurement(homeId:"hent egen nøkkel ved å gå til developer.tibber.com"){
     timestamp
     power
     
@@ -32,7 +42,7 @@ query2 = """
 mutation{
   sendPushNotification(input: {
     title: "Advarsel! Høyt forbruk",
-    message: "Du bruker 8kw eller mer",
+    message: "Du bruker 8kw eller mer", 
     screenToOpen: CONSUMPTION
   }){
     successful
@@ -40,14 +50,13 @@ mutation{
   }
 }
 """
-async def sleep():
-    await asyncio.sleep(300)
 
 async def send_push_notification(data):
     #maybe update your query with the received data here
-    await client.execute_async(query=query2,headers={'Authorization': "5RVds5Cr_tawIiIBb6Itmi6BbVah54xszU62aqA94yQ"}) #pass whatever other params you need
+    await client.execute_async(query=query2,headers={'Authorization': "generer egen nøkkel via developer.tibber.com"})
+    
 
 async def main():
-    await client.subscribe(query=query, headers={'Authorization': "5RVds5Cr_tawIiIBb6Itmi6BbVah54xszU62aqA94yQ"}, handle=print_handle)
+    await client.subscribe(query=query, headers={'Authorization': "samme nøkkel som ovenfor"}, handle=print_handle)
     
 asyncio.run(main())
